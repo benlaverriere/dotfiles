035cdc0c87a567988f069ac64f071f1dd8f9b162
Ben LaVerriere <benlaverriere@gmail.com>
2020-11-20 20:20:13 -0500

    Switch to chruby/ruby-install; add Bundler

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -15,6 +15,15 @@ script/switch_zsh.sh
 # Finally, use `stow` to symlink the config directories in this repository to your home directory:
 script/stow_it_all.sh
 
+# Set up a Ruby installation that's entirely within our control, inspired by https://github.com/monfresh/install-ruby-on-macos
+ruby-install ruby
+chruby ruby
+gem update --system
+gem install bundler && gem update bundler
+number_of_cores=$(sysctl -n hw.ncpu)
+bundle config --global jobs $((number_of_cores - 1))
+bundle install
+
 ## Application/tool-specific followup actions
 
 "$(brew --prefix)/opt/fzf/install"
