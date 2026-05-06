8b96553c6245e849404ef657be2e53220f7d41f1
Ben LaVerriere <ben.laverriere@bookbub.com>
2026-05-06 09:36:51 -0400

    Install git-format-staged with Homebrew

--- script/stow_it_all.sh
+++ script/stow_it_all.sh
@@ -12,9 +12,5 @@ stow vim
 stow iterm
 stow misc
 
-# don't love this: ~/bin comes from ./shared_shell/bin, so we have to .gitignore this binary specifically.
-# (the reason for symlinking it in the first place is because $PATH doesn't recurse.)
-ln -f -s "$(pwd)/src_not_stowed/git-format-staged/git-format-staged" "$HOME/bin"
-
 stow --dir ./qmk --target ./qmk/zsa_qmk_firmware/keyboards keyboards
 ln -f -s "$(pwd)/qmk/qmk.ini" "$HOME/Library/Application Support/qmk"
